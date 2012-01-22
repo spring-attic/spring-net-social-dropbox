2da0f6404a18f277c3e284105104e6aaaf27f86a
Bruno Baia <brbaia@gmail.com>
2012-01-22 17:15:09 +0100

    Added Windows Phone quick start

---
+++ examples/Spring.WindowsPhoneQuickStart/Spring.WindowsPhoneQuickStart/ViewModel/DropboxViewModel.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.IsolatedStorage;
+
+using Spring.Social.OAuth1;
+using Spring.Social.Dropbox.Api;
+
+namespace Spring.WindowsPhoneQuickStart.ViewModel
+{
+    public class DropboxViewModel : INotifyPropertyChanged
+    {
+        private const string OAuthTokenKey = "OAuthToken";
+        public const string CallbackUrl = "http://localhost/Dropbox/Callback";
+
+        private OAuthToken requestOAuthToken;
+        private Uri authenticateUri;
+        private DropboxProfile profile;
+
+        public IOAuth1ServiceProvider<IDropbox> DropboxServiceProvider { get; set; }
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return this.OAuthToken != null;
+            }
+        }
+
+        public OAuthToken OAuthToken
+        {
+            get
+            {
+                return this.LoadSetting<OAuthToken>(OAuthTokenKey, null);
+            }
+            set
+            {
+                this.SaveSetting(OAuthTokenKey, value);
+                NotifyPropertyChanged("IsAuthenticated");
+            }
+        }
+
+        public Uri AuthenticateUri
+        {
+            get
+            {
+                return this.authenticateUri;
+            }
+            set
+            {
+                this.authenticateUri = value;
+                NotifyPropertyChanged("AuthenticateUri");
+            }
+        }
+
+        public DropboxProfile Profile
+        {
+            get
+            {
+                return this.profile;
+            }
+            set
+            {
+                this.profile = value;
+                NotifyPropertyChanged("Profile");
+            }
+        }
+
+        public void Authenticate()
+        {
+            this.DropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(CallbackUrl, null,
+                r =>
+                {
+                    this.requestOAuthToken = r.Response;
+                    OAuth1Parameters parameters = new OAuth1Parameters();
+                    parameters.CallbackUrl = CallbackUrl;
+                    this.AuthenticateUri = new Uri(this.DropboxServiceProvider.OAuthOperations.BuildAuthenticateUrl(r.Response.Value, parameters));
+                });
+        }
+
+        public void AuthenticateCallback(string verifier)
+        {
+            AuthorizedRequestToken authorizedRequestToken = new AuthorizedRequestToken(this.requestOAuthToken, verifier);
+            this.DropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(authorizedRequestToken, null,
+                r =>
+                {
+                    this.OAuthToken = r.Response;
+                    this.ShowProfile();
+                });
+        }
+
+        public void ShowProfile()
+        {
+            IDropbox dropboxClient = this.DropboxServiceProvider.GetApi(this.OAuthToken.Value, this.OAuthToken.Secret);
+            dropboxClient.GetUserProfileAsync(
+                r =>
+                {
+                    if (r.Error == null)
+                    {
+                        this.Profile = r.Response;
+                    }
+                });
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        private void SaveSetting(string key, object value)
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            if (settings.Contains(key))
+            {
+                settings[key] = value;
+            }
+            else
+            {
+                settings.Add(key, value);
+            }
+        }
+
+        private T LoadSetting<T>(string key, T defaultValue)
+        {
+            var settings = IsolatedStorageSettings.ApplicationSettings;
+            if (!settings.Contains(key))
+            {
+                settings.Add(key, defaultValue);
+            }
+            return (T)settings[key];
+        }
+    }
+}
---
+++ examples/Spring.WindowsPhoneQuickStart/Spring.WindowsPhoneQuickStart/Views/Dropbox.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+using Spring.WindowsPhoneQuickStart.ViewModel;
+
+namespace Spring.WindowsPhoneQuickStart.Views
+{
+    public partial class Dropbox : PhoneApplicationPage
+    {
+        public Dropbox()
+        {
+            InitializeComponent();
+
+            this.DataContext = this.ViewModel;
+            this.ViewModel.PropertyChanged += new PropertyChangedEventHandler(ViewModel_PropertyChanged);
+        }
+
+        public DropboxViewModel ViewModel
+        {
+            get { return App.Current.DropboxViewModel; }
+        }
+
+        // Overrided methods
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (this.ViewModel.IsAuthenticated)
+            {
+                this.ViewModel.ShowProfile();
+            }
+            else
+            {
+                this.ViewModel.Authenticate();
+            }
+        }
+
+        // Event methods
+
+        void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // WebBrowser.Source property not bindable
+            if (e.PropertyName == "AuthenticateUri")
+            {
+                this.WebBrowser.Navigate(this.ViewModel.AuthenticateUri);
+            }
+        }
+
+        private void WebBrowser_Navigating(object sender, NavigatingEventArgs e)
+        {
+            if (this.ViewModel != null)
+            {
+                string url = e.Uri.ToString();
+                if (url.StartsWith(DropboxViewModel.CallbackUrl, StringComparison.OrdinalIgnoreCase))
+                {
+                    string verifier = url.Substring(url.LastIndexOf("oauth_token=") + 15);
+                    this.ViewModel.AuthenticateCallback(verifier);
+                }
+            }
+        }
+    }
+}
