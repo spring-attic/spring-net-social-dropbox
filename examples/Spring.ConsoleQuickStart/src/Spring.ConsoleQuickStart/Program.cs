87ba3a5e160c4c28dc340b164acab925f21edc4b
Omar Salem <omar.gamil.salem@gmail.com>
2013-12-16 19:42:39 +0200

    updated examples folder

--- examples/Spring.ConsoleQuickStart/src/Spring.ConsoleQuickStart/Program.cs
+++ examples/Spring.ConsoleQuickStart/src/Spring.ConsoleQuickStart/Program.cs
@@ -17,6 +17,8 @@ namespace Spring.OAuth1ConsoleQuickStart
         // Set your consumer key & secret here
         private const string DropboxAppKey = TODO;
         private const string DropboxAppSecret = TODO;
+        static string userEmail = TODO;
+        static string userPassword = TODO;
 
         static void Main(string[] args)
         {
@@ -25,25 +27,8 @@ namespace Spring.OAuth1ConsoleQuickStart
                 DropboxServiceProvider dropboxServiceProvider = new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.Full);
 
 #if NET_4_0
-                /* OAuth 1.0 'dance' */
 
-                // Authorization without callback url
-                Console.Write("Getting request token...");
-                OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(null , null).Result;
-                Console.WriteLine("Done");
-
-                OAuth1Parameters parameters = new OAuth1Parameters();
-                //parameters.Add("locale", CultureInfo.CurrentUICulture.IetfLanguageTag); // for a localized version of the authorization website
-                string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(oauthToken.Value, parameters);
-                Console.WriteLine("Redirect user for authorization");
-                Process.Start(authenticateUrl);
-                Console.Write("Press any key when authorization attempt has succeeded");
-                Console.ReadLine();
-
-                Console.Write("Getting access token...");
-                AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
-                OAuthToken oauthAccessToken = dropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
-                Console.WriteLine("Done");
+                OAuthToken oauthAccessToken = dropboxServiceProvider.LinkAccount(userEmail, userPassword);
 
                 /* API */
 
@@ -54,47 +39,47 @@ namespace Spring.OAuth1ConsoleQuickStart
                 Console.WriteLine("Hi " + profile.DisplayName + "!");
 
                 // Use step by step debugging, or not             
-/*
-                DeltaPage deltaPage = dropbox.DeltaAsync(null).Result;
-                Entry createFolderEntry = dropbox.CreateFolderAsync("Spring Social").Result;
-                Entry uploadFileEntry = dropbox.UploadFileAsync(
-                    new AssemblyResource("assembly://Spring.ConsoleQuickStart/Spring.ConsoleQuickStart/File.txt"),
-                    "/Spring Social/File.txt", true, null, CancellationToken.None).Result;
-                FileRef fileRef = dropbox.CreateFileRefAsync("Spring Social/File.txt").Result;
-                Entry copyRefEntry = dropbox.CopyFileRefAsync(fileRef.Value, "Spring Social/File_copy_ref.txt").Result;
-                Entry copyEntry = dropbox.CopyAsync("Spring Social/File.txt", "Spring Social/File_copy.txt").Result;
-                Entry deleteEntry = dropbox.DeleteAsync("Spring Social/File.txt").Result;
-                Entry moveEntry = dropbox.MoveAsync("Spring Social/File_copy.txt", "Spring Social/File.txt").Result;
-                dropbox.DownloadFileAsync("Spring Social/File.txt")
-                    .ContinueWith(task =>
-                    {
-                        Console.WriteLine("File '{0}' downloaded ({1})", task.Result.Metadata.Path, task.Result.Metadata.Size);
-                        // Save file to "C:\Spring Social.txt"
-                        using (FileStream fileStream = new FileStream(@"C:\Spring Social.txt", FileMode.Create))
-                        {
-                            fileStream.Write(task.Result.Content, 0, task.Result.Content.Length);
-                        }
-                    });
-                Entry folderMetadata = dropbox.GetMetadataAsync("Spring Social").Result;
-                IList<Entry> revisionsEntries = dropbox.GetRevisionsAsync("Spring Social/File.txt").Result;
-                Entry restoreEntry = dropbox.RestoreAsync("Spring Social/File.txt", revisionsEntries[2].Revision).Result;
-                IList<Entry> searchResults = dropbox.SearchAsync("Spring Social/", ".txt").Result;
-                DropboxLink shareableLink = dropbox.GetShareableLinkAsync("Spring Social/File.txt").Result;
-                DropboxLink mediaLink = dropbox.GetMediaLinkAsync("Spring Social/File.txt").Result;
-                Entry uploadImageEntry = dropbox.UploadFileAsync(
-                    new AssemblyResource("assembly://Spring.ConsoleQuickStart/Spring.ConsoleQuickStart/Image.png"),
-                    "/Spring Social/Image.png", true, null, CancellationToken.None).Result;
-                dropbox.DownloadThumbnailAsync("Spring Social/Image.png", ThumbnailFormat.Png, ThumbnailSize.Medium)
-                    .ContinueWith(task =>
-                    {
-                        Console.WriteLine("Thumbnail '{0}' downloaded ({1})", task.Result.Metadata.Path, task.Result.Metadata.Size);
-                        // Save file to "C:\Thumbnail_Medium.png"
-                        using (FileStream fileStream = new FileStream(@"C:\Thumbnail_Medium.png", FileMode.Create))
-                        {
-                            fileStream.Write(task.Result.Content, 0, task.Result.Content.Length);
-                        }
-                    });
-*/
+                /*
+                                DeltaPage deltaPage = dropbox.DeltaAsync(null).Result;
+                                Entry createFolderEntry = dropbox.CreateFolderAsync("Spring Social").Result;
+                                Entry uploadFileEntry = dropbox.UploadFileAsync(
+                                    new AssemblyResource("assembly://Spring.ConsoleQuickStart/Spring.ConsoleQuickStart/File.txt"),
+                                    "/Spring Social/File.txt", true, null, CancellationToken.None).Result;
+                                FileRef fileRef = dropbox.CreateFileRefAsync("Spring Social/File.txt").Result;
+                                Entry copyRefEntry = dropbox.CopyFileRefAsync(fileRef.Value, "Spring Social/File_copy_ref.txt").Result;
+                                Entry copyEntry = dropbox.CopyAsync("Spring Social/File.txt", "Spring Social/File_copy.txt").Result;
+                                Entry deleteEntry = dropbox.DeleteAsync("Spring Social/File.txt").Result;
+                                Entry moveEntry = dropbox.MoveAsync("Spring Social/File_copy.txt", "Spring Social/File.txt").Result;
+                                dropbox.DownloadFileAsync("Spring Social/File.txt")
+                                    .ContinueWith(task =>
+                                    {
+                                        Console.WriteLine("File '{0}' downloaded ({1})", task.Result.Metadata.Path, task.Result.Metadata.Size);
+                                        // Save file to "C:\Spring Social.txt"
+                                        using (FileStream fileStream = new FileStream(@"C:\Spring Social.txt", FileMode.Create))
+                                        {
+                                            fileStream.Write(task.Result.Content, 0, task.Result.Content.Length);
+                                        }
+                                    });
+                                Entry folderMetadata = dropbox.GetMetadataAsync("Spring Social").Result;
+                                IList<Entry> revisionsEntries = dropbox.GetRevisionsAsync("Spring Social/File.txt").Result;
+                                Entry restoreEntry = dropbox.RestoreAsync("Spring Social/File.txt", revisionsEntries[2].Revision).Result;
+                                IList<Entry> searchResults = dropbox.SearchAsync("Spring Social/", ".txt").Result;
+                                DropboxLink shareableLink = dropbox.GetShareableLinkAsync("Spring Social/File.txt").Result;
+                                DropboxLink mediaLink = dropbox.GetMediaLinkAsync("Spring Social/File.txt").Result;
+                                Entry uploadImageEntry = dropbox.UploadFileAsync(
+                                    new AssemblyResource("assembly://Spring.ConsoleQuickStart/Spring.ConsoleQuickStart/Image.png"),
+                                    "/Spring Social/Image.png", true, null, CancellationToken.None).Result;
+                                dropbox.DownloadThumbnailAsync("Spring Social/Image.png", ThumbnailFormat.Png, ThumbnailSize.Medium)
+                                    .ContinueWith(task =>
+                                    {
+                                        Console.WriteLine("Thumbnail '{0}' downloaded ({1})", task.Result.Metadata.Path, task.Result.Metadata.Size);
+                                        // Save file to "C:\Thumbnail_Medium.png"
+                                        using (FileStream fileStream = new FileStream(@"C:\Thumbnail_Medium.png", FileMode.Create))
+                                        {
+                                            fileStream.Write(task.Result.Content, 0, task.Result.Content.Length);
+                                        }
+                                    });
+                */
             }
             catch (AggregateException ae)
             {
